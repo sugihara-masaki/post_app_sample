e91411bbf6f43709db3323bf5683d7e7a4935123
Sugihara Masaki <sugihara0227@gmail.com>
2019-11-12 15:13:33 +0900

    Add content to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :image)
+    params.require(:post).permit(:title, :image, :content)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+  has_rich_text :content
 end
