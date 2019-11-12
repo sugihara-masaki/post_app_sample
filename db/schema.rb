57d8bd48d1c551023caf499357d08bd73124d6b9
Sugihara Masaki <sugihara0227@gmail.com>
2019-11-12 14:17:52 +0900

    Add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_051418) do
+ActiveRecord::Schema.define(version: 2019_11_12_051701) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2019_11_11_051418) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
