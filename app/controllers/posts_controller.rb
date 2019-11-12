bf8f9eb0cbd8feb570819f03fdc5368ec402e60f
Sugihara Masaki <sugihara0227@gmail.com>
2019-11-12 14:45:20 +0900

    Add create and show action to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
       @post = current_user.posts.build
     end
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
 end
